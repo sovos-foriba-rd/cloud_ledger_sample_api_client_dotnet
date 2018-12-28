a97e291ecf8a2f5b7ff4bbcfcd6ae7aef555b4fc
foriba-rd <rd@foriba.com>
2018-12-29 00:05:32 +0300

    ilk commit

---
+++ ForibaBulutLedgerSampleProject/frmMain.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ForibaBuluteDefterTestProject
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTcVkn = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.btnSorgula = new System.Windows.Forms.Button();
+            this.txtDefterID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtTcVkn);
+            this.panel1.Controls.Add(this.txtSifre);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtKullanici);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(460, 240);
+            this.panel1.TabIndex = 54;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(124, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "TCKN / VKN";
+            // 
+            // txtTcVkn
+            // 
+            this.txtTcVkn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTcVkn.Location = new System.Drawing.Point(32, 122);
+            this.txtTcVkn.MaxLength = 11;
+            this.txtTcVkn.Name = "txtTcVkn";
+            this.txtTcVkn.Size = new System.Drawing.Size(183, 22);
+            this.txtTcVkn.TabIndex = 0;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(242, 189);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(183, 22);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(242, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 16);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "WS Şifre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(32, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 16);
+            this.label5.TabIndex = 60;
+            this.label5.Text = "WS Kullanıcı Adı";
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullanici.Location = new System.Drawing.Point(32, 189);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(183, 22);
+            this.txtKullanici.TabIndex = 2;
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(32, 276);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(183, 45);
+            this.btnGonder.TabIndex = 4;
+            this.btnGonder.Text = "e-Defter Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // btnSorgula
+            // 
+            this.btnSorgula.Location = new System.Drawing.Point(239, 276);
+            this.btnSorgula.Name = "btnSorgula";
+            this.btnSorgula.Size = new System.Drawing.Size(183, 45);
+            this.btnSorgula.TabIndex = 5;
+            this.btnSorgula.Text = "e-Defter Durum Sorgula";
+            this.btnSorgula.UseVisualStyleBackColor = true;
+            this.btnSorgula.Click += new System.EventHandler(this.btnSorgula_Click);
+            // 
+            // txtDefterID
+            // 
+            this.txtDefterID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtDefterID.Location = new System.Drawing.Point(78, 368);
+            this.txtDefterID.Name = "txtDefterID";
+            this.txtDefterID.Size = new System.Drawing.Size(344, 22);
+            this.txtDefterID.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Defter ID";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 416);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDefterID);
+            this.Controls.Add(this.btnSorgula);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Foriba Bulut e-Defter Test Project";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTcVkn;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Button btnSorgula;
+        private System.Windows.Forms.TextBox txtDefterID;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ForibaBulutLedgerSampleProject/frmMain.cs
@@ -0,0 +1,144 @@
+﻿using ForibaBuluteDefterTestProject.ServiceRef;
+using Ionic.Zip;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForibaBuluteDefterTestProject
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private string GetAuthorization(string username, string pass)
+        {
+            string authorization = username + ":" + pass; //kullanıcı adı ve şifre. aralarında : karakteri olacak.
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(authorization);
+            string base64authorization = Convert.ToBase64String(byteArray);
+
+            return string.Format("Basic {0}", base64authorization);
+        }
+
+        private string GetHashInfo(byte[] file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] aa = md5.ComputeHash(file);
+
+                var hash = BitConverter.ToString(aa).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+
+        private byte[] IncludeZip(string file, string fileName)
+        {
+            byte[] ziplenecekData = Encoding.UTF8.GetBytes(file);
+
+            MemoryStream zipStream = new MemoryStream();
+
+            using (ZipFile zip = new Ionic.Zip.ZipFile())
+            {
+                ZipEntry zipEleman = zip.AddEntry(fileName + ".txt", ziplenecekData);
+
+                zip.Save(zipStream);
+            }
+
+            zipStream.Seek(0, SeekOrigin.Begin);
+            zipStream.Flush();
+
+            zipStream.Position = 0;
+            return zipStream.ToArray();
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string defterPath = Application.StartupPath + @"\doc\12345678011233132433.txt";
+
+                string defter = (File.ReadAllText(defterPath)).Replace("#VKN#", txtTcVkn.Text.Trim());
+
+                byte[] zipData = IncludeZip(defter, Guid.NewGuid().ToString());
+
+                ELedgerCloudServiceClient wsClient = new ELedgerCloudServiceClient();
+
+                using (new System.ServiceModel.OperationContextScope((System.ServiceModel.IClientChannel)wsClient.InnerChannel))
+                {
+                    System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add(HttpRequestHeader.Authorization, GetAuthorization(txtKullanici.Text.Trim(), txtSifre.Text.Trim()));
+
+                    var req = new SaveRequest()
+                    {
+                        company = new Company
+                        {
+                            identifier = txtTcVkn.Text.Trim(),
+                        },
+
+                        source = new LedgerStream
+                        {
+                            fileName = "12345678011233132433.zip",
+                            hash = GetHashInfo(zipData),
+                            binaryData = zipData
+                        }
+                    };
+
+                    var result = wsClient.save(req);
+
+                    txtDefterID.Text = result.ledgerId.ToString();
+
+                    MessageBox.Show(result.message, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSorgula_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long defid;
+
+                if (long.TryParse(txtDefterID.Text.Trim(), out defid))
+                {
+                    ELedgerCloudServiceClient wsClient = new ELedgerCloudServiceClient();
+
+                    using (new System.ServiceModel.OperationContextScope((System.ServiceModel.IClientChannel)wsClient.InnerChannel))
+                    {
+                        System.ServiceModel.Web.WebOperationContext.Current.OutgoingRequest.Headers.Add(HttpRequestHeader.Authorization, GetAuthorization(txtKullanici.Text.Trim(), txtSifre.Text.Trim()));
+
+                        var req = new StatusRequest()
+                        {
+                            company = new Company
+                            {
+                                identifier = txtTcVkn.Text.Trim()
+                            },
+
+                            ledgerId = defid
+                        };
+
+                        var result = wsClient.status(req);
+
+                        MessageBox.Show(result.code + " - " + result.message, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Geçersiz e-Defter ID", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
